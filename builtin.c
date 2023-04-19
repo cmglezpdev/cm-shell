863f8b77c09a0b834785390b2a7f947549038806
Carlos Manuel <cmglezpdev@gmail.com>
2023-04-18 20:22:49 -0400

    feat: add methods to use history and again command(with errors)

--- builtin.c
+++ builtin.c
@@ -15,14 +15,22 @@
 // List builtin commands, followed by their corresponding functions
 char *builtin_str[] = { 
     "cd", 
-    "help",
     "exit" 
+};
+
+char *builtin_str_out[] = { 
+    "help",
+    "history"
 };
 
 int (*builtin_func[]) (char **) = {
     &cmsh_cd,
-    &cmsh_help, 
     &cmsh_exit
+};
+
+int (*builtin_func_out[]) (char **) = {
+    &cmsh_help,
+    &cmsh_history
 };
 
 
@@ -57,6 +65,78 @@ int cmsh_exit(char **args) {
 }
 
 
+int cmsh_history(char** args) {
+    char** history = get_history();
+
+    for(int i = 0; history[i] != NULL; i ++) {
+        printf("%d: %s\n", i + 1, history[i]);
+    }
+    free(history);
+}
+
+
+char** get_history() {
+    int fd = open(CMSH_HISTORY_FILE, O_CREAT | O_RDONLY);
+    char* doc = malloc(CMSH_TOK_BUFF_SIZE);
+    read(fd, doc, CMSH_TOK_BUFF_SIZE);
+    return cmsh_split_line(doc, "\n");
+}
+
+char* get_again(int number) {
+    if( number < 1 || number > 10 ) {
+        perror("cmsh: The command number should be between [1..10]\n");
+        exit(EXIT_FAILURE);
+    }
+    number --;
+
+    char** commands = get_history();
+    char* again = NULL;
+
+    for(int i = 0; commands[i] != NULL; i ++ ) {
+        if( number == i ) {
+            again = malloc(strlen(commands[i]));
+            strcmp(again, commands[i]);
+            break;
+        }
+    }
+
+    if( again == NULL ) {
+        perror("cmsh: The command doesn't exist in the history\n");
+        exit(EXIT_FAILURE);
+    }
+
+    free(commands);
+    return again;
+}
+
+void save_in_history(char *line) {
+    char** commands = get_history();
+    int t;
+    for(t = 0; commands[t] != NULL; t ++);
+    int start = (t < 10) ? 0 : 1;
+    char* history = malloc(CMSH_MAX_BUFFSIZE);
+
+    for(int i = start; i < t; i ++) {
+        strcat(history, commands[i]);        
+        strcat(history, "\n");
+    }
+
+    strcat(history, line);
+
+    int fd = open(CMSH_HISTORY_FILE, O_WRONLY | O_TRUNC);
+    write(fd, history, strlen(history));
+
+    free(history); free(commands);
+    close(fd);
+}
+
+
 int cmsh_num_builtins() {
     return sizeof(builtin_str) / sizeof(char *);
 }
+
+int cmsh_num_builtins_out() {
+    return sizeof(builtin_str_out) / sizeof(char *);
+}
+
+
--- builtin.h
+++ builtin.h
@@ -3,11 +3,24 @@
 #define SHELL_BUILTIN_H
 #endif
 
+// #define CMSH_HISTORY_FILE ".cmsh_history"
+#define CMSH_HISTORY_FILE "history.txt"
 
 // List builtin commands, followed by their corresponding functions
-extern char *builtin_str[3];
+extern char *builtin_str[2];
 
-extern int (*builtin_func[3]) (char **);
+extern int (*builtin_func[2]) (char **);
+
+extern char *builtin_str_out[2];
+
+extern int (*builtin_func_out[2]) (char **);
+
+
+char** get_history();
+
+char* get_again(int number);
+
+void save_in_history(char *line);
 
 // Function Declarations for builtin shell commands:
 int cmsh_cd(char **args);
@@ -16,4 +29,8 @@ int cmsh_help(char **args);
 
 int cmsh_exit(char **args);
 
+int cmsh_history(char **args);
+
 int cmsh_num_builtins();
+
+int cmsh_num_builtins_out();
